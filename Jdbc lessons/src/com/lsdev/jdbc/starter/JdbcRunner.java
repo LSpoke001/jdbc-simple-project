720237a43174ebd9d435b1b56b96c51a09a9229e
Elena Spitsyna <lspoke.larn@gmail.com>
2024-10-21 17:26:31 +0300

    Simple statement

--- Jdbc lessons/src/com/lsdev/jdbc/starter/JdbcRunner.java
+++ Jdbc lessons/src/com/lsdev/jdbc/starter/JdbcRunner.java
@@ -10,8 +10,19 @@ public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
 
-        try (var connection = ConnectionManager.open()) {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS info(
+                    id SERIAL PRIMARY KEY,
+                    data TEXT NOT NULL
+                ) ;
+                """;
+
+        try (var connection = ConnectionManager.open();
+            var statement = connection.createStatement()) {
+            System.out.println(connection.getSchema()); // получение текущей схемы
             System.out.println(connection.getTransactionIsolation());
+            var executeResult = statement.execute(sql);
+            System.out.println(executeResult);
         }
     }
 }
